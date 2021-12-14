36226f7587b4a3f896ad20467a6f2c93365587ce
Arunsiddardha <ai20btech11019@iith.ac.in>
2021-12-14 20:50:23 +0530

    Done

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
-
 //connection to database
 const dbURI = keys.mongo.code;
 mongoose
@@ -37,10 +36,10 @@ app.use(
 app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("signup", { user: req.user });
 });
 app.get("/signup", (req, res) => res.render("signup"));
 app.use(route);
-app.get("*",(req,res) => res.send("404 error page Not Found"));
+app.get("*", (req, res) => res.send("404 error page Not Found"));
