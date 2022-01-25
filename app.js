59bbcef2405e586120316294a2458a8edc7ec197
Arunsiddardha <ai20btech11019@iith.ac.in>
2022-01-25 19:25:55 +0530

    added backendupdating feature

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose
     UseUNifiedTopology: true,
   })
   .then((result) => {
-    app.listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 80);
     console.log("connected");
   })
   .catch((err) => console.log(err));
